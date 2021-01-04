7d5ae9ad75b409f49c249059ca920220c6380e16
awirut3526293 <61749500+awirut3526293@users.noreply.github.com>
2021-01-04 13:11:29 +0700

    Update compound interest.cpp

--- compound interest.cpp
+++ compound interest.cpp
@@ -5,212 +5,171 @@
 
  double p, r, k, n, base, h, i, s, a, base1;
 
-findS ()
-{
+findS ()	
+	{
   
- 
-base = (k + r) / k;
+		base = (k + r) / k;
   
-base1 = (k + r) / k;
+		base1 = (k + r) / k;
   
-a = k * n;
+		a = k * n;
   
-for (i = 1; i < a; i++)
-    {
-      base = base * base1;
-    }
+		for (i = 1; i < a; i++)
+    		{
+      			base = base * base1;
+   	 	    }
   
-s = p * base;
+		s = p * base;
   
-printf ("S = ");
-  
-printf ("%.4f\n\n", s);
-
- 
- 
+			printf ("S = ");
+			printf ("%.4f\n\n", s);
 }
 
-
- 
- 
 findP ()
-{
+		{
   
-base = (k + r) / k;
+		base = (k + r) / k;
   
-base1 = (k + r) / k;
+		base1 = (k + r) / k;
   
-a = k * n;
+		a = k * n;
   
-for (i = 1; i < a; i++)
-    {
-      base = base * base1;
-    }
+		for (i = 1; i < a; i++)
+    		{	
+      			base = base * base1;
+   			 }
   
-p = s / (base);
+		p = s / (base);
   
-printf ("P = ");
-  
-printf ("%.4f\n\n", p);
-
-}
-
-
- 
+		printf ("P = "); 
+		printf ("%.4f\n\n", p);
+	}
+	
 findN ()
-{
+	{
   
-double up, dn;
+		double up, dn;
   
-base = (k + r) / k;
+		base = (k + r) / k;
   
-up = log10 (s / p);
+		up = log10 (s / p);
   
-dn = log10 (base);
+		dn = log10 (base);
   
-n = (up) / (k * dn);
+		n = (up) / (k * dn);
   
-printf ("n = ");
+		printf ("n = ");
+		printf ("%.4f\n\n", n);
+	} 
+ 
+	main ()
+	{
   
-printf ("%.4f\n\n", n);
+		int a;
+		st:
+			
+		printf ("******************  //MENU//\n");
+  
+		printf ("*         r\t * Find S(1)\n");
+  
+		printf ("*S = P(1+ - )^kn * Find P(2)\n");
+  
+		printf
+    	("*         k\t * Find n(3) \n****************** Reset(4)Exit(5+)\n--Input(1-4)--\n");
+  
+ 
+		scanf ("%d", &a);
+		switch (a) {
+    
+			case 1:    {	
+						printf ("**** S calculator****\n");
 
-} 
- 
- 
-main ()
-{
-  
-int a;
-
-st:
-printf ("******************  //MENU//\n");
-  
-printf ("*         r\t * Find S(1)\n");
-  
-printf ("*S = P(1+ - )^kn * Find P(2)\n");
-  
-printf
-    ("*         k\t * Find n(3) \n****************** New(4)Exit(5+)\n--Input(1-4)--\n");
-  
- 
-scanf ("%d", &a);
-  
-switch (a)
-    {
+						printf ("Input P: ");
+	
+						scanf ("%lf", &p);
+	
+						/*	printf("Input S: ");
+							scanf("%lf",&s);*/ 
+	  					printf ("Input r: ");
+	
+						scanf ("%lf", &r);
+	
+						printf ("Input k: ");
+							
+						scanf ("%lf", &k);
+							
+						printf ("Input n: ");
+							
+						scanf ("%lf", &n);
+							
+						findS ();								
+						break;      
+					}
     
-case 1:
-      {
+			case 2: {
 	
-printf ("**** S calculator****\n");
+						printf ("**** P calculator****\n");
 	
-printf ("Input P: ");
+	  					/*  printf("Input P: "); 
+	     				/scanf("%lf",&p); */ 
+	 					 printf ("Input S: ");
 	
-scanf ("%lf", &p);
-	
- printf ("Input r: ");
-	
-scanf ("%lf", &r);
-	
-printf ("Input k: ");
-	
-scanf ("%lf", &k);
-	
-printf ("Input n: ");
-	
-scanf ("%lf", &n);
-	
-findS ();
-	
-break;
-      
-}
+						scanf ("%lf", &s);
+							
+						printf ("Input r: ");
+							
+						scanf ("%lf", &r);
+							
+						printf ("Input k: ");
+							
+						scanf ("%lf", &k);
+							
+						printf ("Input n: ");
+							
+						scanf ("%lf", &n);
+							
+						findP ();
+						break;					      
+					}
     
-case 2:
-      {
+			case 3:	{
 	
-printf ("**** P calculator****\n");
-printf ("Input S: ");
-	
-scanf ("%lf", &s);
-	
-printf ("Input r: ");
-	
-scanf ("%lf", &r);
-	
-printf ("Input k: ");
-	
-scanf ("%lf", &k);
-	
-printf ("Input n: ");
-	
-scanf ("%lf", &n);
-	
-findP ();
-	
- 
-break;
-      
-}
+						printf ("**** n calculator****\n");
+							
+						printf ("Input P: ");
+							
+						scanf ("%lf", &p);
+							
+						printf ("Input S: ");
+							
+						scanf ("%lf", &s);
+							
+						printf ("Input r: ");
+							
+						scanf ("%lf", &r);
+							
+						printf ("Input k: ");
+							
+						scanf ("%lf", &k);
+							
+						/*	printf("Input n: ");
+						scanf("%lf",&n);*/ 
+						findN ();
+						break;					      
+					}
     
-case 3:
-      {
-	
-printf ("**** n calculator****\n");
-	
-printf ("Input P: ");
-	
-scanf ("%lf", &p);
-	
-printf ("Input S: ");
-	
-scanf ("%lf", &s);
-	
-printf ("Input r: ");
-	
-scanf ("%lf", &r);
-	
-printf ("Input k: ");
-	
-scanf ("%lf", &k);
-	
-/*	printf("Input n: ");
-		scanf("%lf",&n);*/ 
-	  findN ();
-	
- 
-break;
-      
-}
-    
-case 4:
-      {
-	
-goto st;
-	
-break;
-      
-}
-    
-case 5:
-      {
-	
-goto end;
-	
-break;
-      
-}
-    
-default:
-      goto end;
-    
- 
-}
+			case 4: {
+					goto st;	
+					break;
+      				}
+    	  
+			default:
+      			goto end;
+    				}
   
-goto st;
-
-end:;
+		goto st;
+		end:;
 
  
 }
-
 
